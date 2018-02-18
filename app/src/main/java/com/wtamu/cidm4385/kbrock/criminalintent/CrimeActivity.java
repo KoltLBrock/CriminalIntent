343ac84820aa35c490c563343da23bae5c4ea803
KoltLBrock <35542746+KoltLBrock@users.noreply.github.com>
2018-02-18 16:40:24 -0600

    Initial commit, ch8 no challenge

---
+++ app/src/main/java/com/wtamu/cidm4385/kbrock/criminalintent/CrimeActivity.java
@@ -0,0 +1,11 @@
+package com.wtamu.cidm4385.kbrock.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
---
+++ app/src/main/java/com/wtamu/cidm4385/kbrock/criminalintent/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package com.wtamu.cidm4385.kbrock.criminalintent;
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
